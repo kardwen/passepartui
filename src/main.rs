1295b2245daf84d79531e8f3c51c35cc14989b35
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-01-24 15:44:19 +0000

    Hide the feature under the cli argument --set-title

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use ratatui::crossterm::{
     event::{DisableMouseCapture, EnableMouseCapture},
-    execute, terminal::SetTitle,
+    execute,
+    terminal::SetTitle,
 };
 use std::{env, io::stdout};
 
@@ -15,9 +16,13 @@ use app::App;
 
 fn main() -> Result<()> {
     let tty_pinentry = env::args().any(|arg| arg == "--tty-pinentry");
+    // See: https://github.com/kardwen/passepartui/issues/12#issuecomment-2612471345
+    let set_title = env::args().any(|arg| arg == "--set-title");
 
     let mut terminal = ratatui::init();
-    execute!(stdout(), SetTitle("passepartui"))?;
+    if set_title {
+        execute!(stdout(), SetTitle("passepartui"))?;
+    }
     execute!(stdout(), EnableMouseCapture)?;
     terminal.clear()?;
     let result = App::new(tty_pinentry).run(&mut terminal);
